48a27a2733937e597bdaafb917690a0f56654f1b
Amhjadkhan <amjadpogramer@gmail.com>
2023-09-27 11:45:55 +0400

    this is pie chart commit

--- donation/src/Pages/Statistics/Statistics.jsx
+++ donation/src/Pages/Statistics/Statistics.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Chart } from 'react-google-charts';
 
 function Statistics() {
+  const [donationItems, setDonationItems] = useState([]);
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    const storedDonations = JSON.parse(localStorage.getItem('donation'));
+    if (storedDonations) {
+      setDonationItems(storedDonations);
+      calculatePercentage(storedDonations);
+    }
+  }, []);
+
+  const calculatePercentage = (donations) => {
+    const donationLength = donations.length;
+    const remainingPercentage = (donationLength / donationItems.length) * 100;
+    setPercentage(remainingPercentage);
+  };
+
+  const data = [
+    ['Donated', 'Amount', { role: 'style' }],
+    ['Donation', donationItems.length, 'green'], 
+    ['Remaining', 100 - percentage, 'red'], 
+  ];
+
+  const options = {
+    title: 'Donation Statistics',
+    sliceVisibilityThreshold: 0.2,
+  };
+
   return (
     <div>
       <h1>Statistics</h1>
+      <Chart
+        chartType="PieChart"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+      />
     </div>
-  )
+  );
 }
 
-export default Statistics
+export default Statistics;
